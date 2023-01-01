78f12a15ee37c49cafbfd5c62db5aa43de05a6c3
Alfred Polycarpe <alfred@polycarpe.com>
2023-01-01 17:30:23 +0100

    config

--- config/database.js
+++ config/database.js
@@ -12,29 +12,28 @@
 
 module.exports = ({ env }) => ({
   connection: {
+    default: {
+    connector: 'bookshelf',
+    settings: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "strapi"),
+          user: env("DATABASE_USERNAME", ""),
+          password: env("DATABASE_PASSWORD", ""),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        options: {
+          ssl: true,
+        },
+        debug: false,
+        useNullAsDefault: true,
       },
     },
-    pool: {
-      min: 0,
-      max: 4,
-      acquireTimeoutMillis: 300000,
-      createTimeoutMillis: 300000,
-      destroyTimeoutMillis: 300000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis:1000,
-      createRetryIntervalMillis: 2000
-    },
-    debug: false,
-    useNullAsDefault: true,
   },
 });
