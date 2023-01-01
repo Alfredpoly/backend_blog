c95a19b35f93e560fba2c875e8544efe20361ea4
Alfred Polycarpe <alfred@polycarpe.com>
2023-01-01 19:00:57 +0100

    config

--- config/database.js
+++ config/database.js
@@ -14,25 +14,15 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "strapi.c9jwteiydysx.eu-west-3.rds.amazonaws.com"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "postgres"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "Testtest2"),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host: process.env("DATABASE_HOST", "strapi.c9jwteiydysx.eu-west-3.rds.amazonaws.com"),
+      port: process.env.int("DATABASE_PORT", 5432),
+      database: process.env("DATABASE_NAME", "postgres"),
+      user: process.env("DATABASE_USERNAME", "postgres"),
+      password: process.env("DATABASE_PASSWORD", ""),
+      // schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
-    },
-    pool: {
-      min: 2,
-      max: 40,
-      createTimeoutMillis: 3000,
-      acquireTimeoutMillis: 30000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 100,
-      propagateCreateError: false // <- default is true, set to false
     },
     debug: false,
     useNullAsDefault: true,
--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('0.0.0.0'),
+  host: env('HOST', '0.0.0.0'),
   port: process.env.PORT,
-  // app: {
-  //   keys: env.array('APP_KEYS'),
-  // },
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
   url: env('MY_HEROKU_URL')
 });
