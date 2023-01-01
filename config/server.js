8ebeeefaa4151f8becccf52a63634a84cd463b6c
Alfred Polycarpe <alfred@polycarpe.com>
2023-01-01 17:03:05 +0100

    config

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080),
+  port: process.env.PORT,
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: 'https://cms.alfredpolycarpe.com'
+  // url: 'https://cms.alfredpolycarpe.com'
 });
