d84334286a574759ebe1d32e9bd88282b0fccb38
Alfred Polycarpe <alfred@polycarpe.com>
2023-01-01 17:33:12 +0100

    config

--- config/database.js
+++ config/database.js
@@ -12,28 +12,19 @@
 
 module.exports = ({ env }) => ({
   connection: {
-    default: {
-    connector: 'bookshelf',
-    settings: {
     client: "postgres",
     connection: {
-      
-          host: env("DATABASE_HOST", "127.0.0.1"),
-          port: env.int("DATABASE_PORT", 5432),
-          database: env("DATABASE_NAME", "strapi"),
-          user: env("DATABASE_USERNAME", ""),
-          password: env("DATABASE_PASSWORD", ""),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
-        },
-        options: {
-          ssl: true,
-        },
-        debug: false,
-        useNullAsDefault: true,
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
     },
+    debug: false,
+    useNullAsDefault: true,
   },
 });
