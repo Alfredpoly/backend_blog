99f03665609d3e3b3d9634eb9299a71550bc15af
Alfred Polycarpe <alfred@polycarpe.com>
2023-01-01 17:43:26 +0100

    config

--- config/database.js
+++ config/database.js
@@ -14,11 +14,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "strapi.c9jwteiydysx.eu-west-3.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      database: env("DATABASE_NAME", "postgres"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "Testtest2"),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
