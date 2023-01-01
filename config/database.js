c03698710ade9100f667059f4e1f7bca8fde527d
Alfred Polycarpe <alfred@polycarpe.com>
2023-01-01 17:39:17 +0100

    config

--- config/database.js
+++ config/database.js
@@ -24,6 +24,16 @@ module.exports = ({ env }) => ({
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
     },
+    pool: {
+      min: 2,
+      max: 6,
+      createTimeoutMillis: 3000,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+      propagateCreateError: false // <- default is true, set to false
+    },
     debug: false,
     useNullAsDefault: true,
   },
